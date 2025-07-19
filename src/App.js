ca05b9f9a5f350557f1b5a0affd9fa4ec425624d
Arkashish Sutradhar <arkashishdhar1@gmail.com>
2025-07-19 22:30:49 +0530

    Completed project -- Boards not working as expected due to outdated package

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Layout from './Components/Layouts/Layout';
 import Dashboard from './Pages/Dashboard/Dashboard';
 import Calendar from './Pages/Calendar/Calendar';
+import Board from './Pages/Board/Board';
+import DataGrid from './Pages/DataGrid/DataGrid';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
@@ -16,6 +18,8 @@ function App() {
           <Route path='/' element={<Layout />}>
             <Route path='Dashboard' element={<Dashboard />} />
             <Route path='Calendar' element={<Calendar />} />
+            <Route path='Board' element={<Board />} />
+            <Route path='DataGrid' element={<DataGrid />} />
           </Route>
 
         </Routes>
